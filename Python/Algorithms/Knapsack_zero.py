f5a5214c5c3d1c90872a12eacb88e3d2d8a4a09a
ihobbang250 <ihobbang250@gmail.com>
2022-12-01 19:05:11 +0900

    1

---
+++ Python/Algorithms/Knapsack_zero.py
@@ -0,0 +1,12 @@
+bagSize = int(input())
+itemNumber = int(input())
+itemSize = list(int(input()).split(" "))
+itemValue = list(int(input()).split(" "))
+x = []
+def p(v)
+def knapsack (i, size):
+    if i > n or size <= 0:
+        print(x)
+        return
+    
+    p(v) = sum(P[j] for j in range(1, i) if x[j] == 1)

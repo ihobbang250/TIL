305f20c907de09155451c80950b8337c8daf7c57
ihobbang250 <ihobbang250@gmail.com>
2022-12-02 04:36:19 +0900

    test

--- Python/Algorithms/Knapsack_zero.py
+++ Python/Algorithms/Knapsack_zero.py
@@ -1,12 +1,57 @@
 bagSize = int(input())
 itemNumber = int(input())
-itemSize = list(int(input()).split(" "))
-itemValue = list(int(input()).split(" "))
-x = []
-def p(v)
+itemSize = list(map(int, input().split()))
+itemProfit = list(map(int, input().split()))
+itemSize.insert(0, 0)
+itemProfit.insert(0, 0)
+
+
+x = [0 for i in range(bagSize)]
+global MaxProfit
+MaxProfit = 0
+global frac_max
+frac_max = 0
+perform = 0
+k = {}
+
+for j in range(i, itemNumber):
+    perform = itemProfit[j] / itemSize[j]
+    k[itemSize[j]] = perform
+sorted_k = sorted(k.items(), key= lambda x: x[1])
+
+
 def knapsack (i, size):
-    if i > n or size <= 0:
+    global MaxProfit
+    if i > itemNumber or size <= 0:
         print(x)
         return
     
-    p(v) = sum(P[j] for j in range(1, i) if x[j] == 1)
+    p = sum(itemProfit[j] for j in range(0, i) if x[j] == 1)
+    s = sum(itemSize[j] for j in range(0, i) if x[j] == 1)
+    Bag = frac_knapsack(i + 1, size - s)
+    
+    #Case x[i] = 1 
+    if itemSize[i] <= size:
+        Bag = frac_knapsack(i + 1, size - itemSize[i])
+        if Bag == None:
+            Bag = frac_knapsack(i, size - itemSize[i])
+        if p + itemProfit[i] + Bag > MaxProfit:
+            MaxProfit = p + itemProfit[i]
+        x[i] = 1
+        knapsack(i + 1, size - itemSize[i])
+
+    #Case x[i] = 0
+    Bag = frac_knapsack(i + 1, size)
+    if p + Bag > MaxProfit:
+        x[i] = 0
+        knapsack(i + 1, size)           
+            
+def frac_knapsack (i, size):
+    global frac_max
+    
+    if i > itemNumber or size <= 0:
+        return None
+    
+    
+    
+        

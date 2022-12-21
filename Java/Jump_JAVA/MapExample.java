271b8b7ef3cecebb9ce19cf96daf3c650863446c
ihobbang250 <ihobbang250@gmail.com>
2022-12-21 12:55:56 +0900

    up

--- Java/Jump_JAVA/MapExample.java
+++ Java/Jump_JAVA/MapExample.java
@@ -1,11 +1,7 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class MapExample {
     public static void main(String[] args) {
-        HashMap<String, Integer> a = new HashMap<>();
-        a.put("A", 90);
-        a.put("B", 80);
-        a.put("C", 70);
-        System.out.println(a.get("C"));
+        
     }
 }

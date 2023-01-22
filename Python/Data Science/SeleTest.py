f98a029a66187192049b25051825ed8423d44311
ihobbang250 <ihobbang250@gmail.com>
2023-01-22 15:15:20 +0900

    selenium study

--- Python/Data Science/SeleTest.py
+++ Python/Data Science/SeleTest.py
@@ -1,19 +1,16 @@
 from selenium import webdriver
 import time
-
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging']) # Usb Error ignore
-options.add_argument("window-size=1000,1000")
 options.add_argument("no-sandbox") 
 
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-browser.get("https://naver.com")
 browser.get("https://shopping.naver.com")
-browser.back()
-browser.forward()
-time.sleep(3)
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "fieldset input[type=text]")))
 browser.close()

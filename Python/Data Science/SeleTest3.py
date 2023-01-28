7889329616036af2e947326d288512b4cb22f112
ihobbang250 <ihobbang250@gmail.com>
2023-01-28 17:22:53 +0900

    up

--- Python/Data Science/SeleTest3.py
+++ Python/Data Science/SeleTest3.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import os
+import re
 
-options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions() 
 options.add_experimental_option('excludeSwitches', ['enable-logging']) # Usb Error ignore
 options.add_argument("no-sandbox") 
 
@@ -20,7 +20,7 @@ wait = WebDriverWait(browser, 10)
 def find_present(css):
     return wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css)))
 
-def finds_present(css): # Find all include selector 
+def find_presents(css): # Find all include selector 
     find_present(css)
     return browser.find_elements(By.CSS_SELECTOR, css)
 
@@ -29,7 +29,7 @@ def find_visible(css):
     return wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, css)))
 
 
-def finds_visible(css): # Find all include selector 
+def find_visibles(css): # Find all include selector 
     find_visible(css)
     return browser.find_elements(By.CSS_SELECTOR, css)
 
@@ -39,6 +39,8 @@ def choose_one(text, options):
     print(text)
     print("--------------------------------")
     for i in range(len(options)):
+        if i == 10:
+            break
         print(f"{i+1}. {options[i].text}")
     choice = int(input("-> "))
     options[choice-1].click()
@@ -65,33 +67,44 @@ cpu.click()
 time.sleep(1)
 
 # cpu company
-options = finds_visible("input[name=makerCode]+span")
-i = choose_one("CPU 제조사 번호를 선택", options)
-
-# cpu version
-options = finds_visible("div.search_option_item")
-if i == "인텔": 
-    options[1].find_elements(By.CSS_SELECTOR, "button")[0].click()
-else:
-    options[2].find_elements(By.CSS_SELECTOR, "button")[0].click()
-
-options = finds_visible("div[class$=open] span.item_text")
-choose_one("CPU 종류를 선택", options)
+options = find_visibles("input[name=makerCode]+span")
+maker = choose_one("CPU 제조사 번호를 선택", options)
 time.sleep(1)
 
-# cpu list
-products = finds_visible("div.scroll_box tr[class^=productList]")
+# cpu version
+# options = find_visibles("div.search_option_item")
+# if maker == "인텔": 
+#     options[1].find_elements(By.CSS_SELECTOR, "button")[0].click()
+# else: #AMD
+#     options[2].find_elements(By.CSS_SELECTOR, "button")[0].click()
 
+# options = find_visibles("div[class$=open] span.item_text")
+# choose_one("CPU 종류를 선택", options)
+
+# cpu list
+products = find_visibles("div.scroll_box tr[class^=productList]")
+products_list = []
 for idx, p in enumerate(products):
     name = p.find_element(By.CSS_SELECTOR, "p.subject a").text
+    name = re.sub(r"\(.* ?\)", "", name)
+    socket = p.find_element(By.CSS_SELECTOR, "a.spec").text.split("/")[0]
     try:
         price = p.find_element(By.CSS_SELECTOR, "span.prod_price").text
     except:
-        print("재고없음")
-        continue
-    print(f"{idx+1}. {name}, -----{price}")
+        price = None
+    products_list.append([name, price])
+    print(f"{idx+1}. {name}/ {socket} / {price}")
 
-# mainboard = find_visible(category_css["Mainboard"])
+# # cpu socket
+# spec = find_visible("div.scroll_box div.spec_bg a").text
+# socket = spec.split("/")[0]
+
+
+# performance = spec.split("/")[-2].split(":")[1]
+# performance_number = int(re.sub(r'[^0-9]', "", performance))
+
+# print(socket, performance_number)
+# # mainboard = find_visible(category_css["Mainboard"])
 # mainboard.click()
 
 time.sleep(5)

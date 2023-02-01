226e0a19ff65e8f512fa8dc9bda11b75543bd5d5
ihobbang250 <ihobbang250@gmail.com>
2023-02-01 13:11:14 +0900

    up

--- Python/Data Science/SeleTest3.py
+++ Python/Data Science/SeleTest3.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
